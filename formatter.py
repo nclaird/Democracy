4ae3eed8a0c4213397714eec1f25ebd8d61d48d9
gdbiles <gdbiles@gmail.com>
2016-06-05 23:07:36 -0700

    fix agg_data.py

--- formatter.py
+++ formatter.py
@@ -4,22 +4,14 @@ import math
 
 data = json.load(open('data/aggregate/trump_7day.json'))
 
-
 streams = {s: [] for s in ['official', 'twitter', 'facebook', 'google', 'donations']}
 
-
-
 for date in data:
-
     for stream in data[date]:
-
         streams[stream].append({
             "date": date,
             "value": data[date][stream]
         })
-
-
-
 
 f = open('finalData.json', 'w+')
 json.dump({"streams": streams}, f)
