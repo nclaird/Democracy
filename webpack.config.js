980f1776cd397f2f24875bd433489d7e9af5c773
Drew Moore <drewmoore@ucsc.edu>
2016-06-08 23:21:50 -0700

    webapp infrastructure - typescript/sass/webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+
+
+
+var config = {
+    entry: './src/main.ts',
+    output: {
+        filename: './dist/bundle.js'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['', '.ts', '.js']
+    },
+    module: {
+        loaders: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            {test: /\.ts$/, loader: 'awesome-typescript-loader'},
+            {test:/\.(scss|css)$/, loader: ExtractTextPlugin.extract('style', 'css!sass')
+},
+            { test: /\.html/, loader: 'html?name=[name].[ext]' }
+
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+         new ExtractTextPlugin("styles.css")
+        
+    ],
+    
+    devServer: {
+        contentBase: 'src/',
+        historyApiFallback: true,
+        stats: 'minimal'
+    },
+    devtool: 'source-map'
+};
+
+
+module.exports = config;
